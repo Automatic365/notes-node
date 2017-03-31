bb14a214587dbc1c76d7dece3c65c04343f7c9b4
Jason Hanna <jason.hanna01@gmail.com>
2017-03-31 12:07:47 -0600

    refactoring

--- notes.js
+++ notes.js
@@ -1,31 +1,33 @@
 console.log("Starting notes.js")
 
 const fs = require('fs')
+var fetchNotes = () => {
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    return JSON.parse(noteString);
+  } catch (e){
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
 
 var addNote = function(title, body){
-  var notes = []
+  var notes = fetchNotes()
   var note = {
     title,
     body
-  }
+  };
 
-  try {
-    var noteString = fs.readFileSync('notes-data.json')
-    notes = JSON.parse(noteString)
-  } catch (e){
-
-  }
-
-  var duplicateNotes = notes.filter((note => note.title === title))
-  
+  var duplicateNotes = notes.filter((note => note.title === title));
 
   if (duplicateNotes.length === 0){
-    notes.push(note)
-    stringNotes = JSON.stringify(notes)
-    fs.writeFileSync("notes-data.json", stringNotes)
+    notes.push(note);
+    saveNotes(notes);
   }
-
-}
+};
 
 
 module.exports = {
